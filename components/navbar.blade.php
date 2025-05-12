3f8b72cc6c9bd553f328e98b07bd3d27e284a541
Ahmad <contact@ahmad.tech>
2025-05-12 09:41:28 +0300

    navbar classes

--- components/navbar.blade.php
+++ components/navbar.blade.php
@@ -2,10 +2,11 @@
     'center' => null,
     'end' => null,
     'container' => 'container ',
+    'classes' => '',
 ])
 <div {{ $attributes->class('bg-white py-2 px-2 xl:px-0') }}>
     <div class="{{$container}} mx-auto flex items-center justify-between">
-        <div class="flex items-center">
+        <div class="flex items-center {{$classes}}">
             {{$slot}}
         </div>  
 
